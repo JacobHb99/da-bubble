7454351ba9a954a91dd4c01a69e7414d0e13c9a9
Sebastian Torney <STorney@web.de>
2024-10-24 22:52:09 +0200

    start channel-chat+Main Scss mixin dFlex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChannelChatComponent } from "./main/channel-chat/channel-chat.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChannelChatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
