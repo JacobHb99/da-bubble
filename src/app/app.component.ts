db5a6fce983acdc21550eb60bb42a5ab07ac28a0
JanThorwarth <thorwarth.jan@web.de>
2024-10-25 09:00:35 +0200

    created header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChannelChatComponent } from "./main/channel-chat/channel-chat.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChannelChatComponent],
+  imports: [RouterOutlet, ChannelChatComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
