12fb4f942eee573ca440bd4c0e4b511170ee7d15
Jacob Hengsbach <jacob.hb@live.de>
2024-10-25 18:52:55 +0200

    header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,11 @@
 
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import {Component, inject} from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import { AuthService } from '../services/auth.service';
-=======
 import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from './../services/auth.service';
->>>>>>> 99e039d64768339d96d5f6bb34cb97fd62edc1db
 
 
 @Component({
@@ -25,9 +16,5 @@ import { AuthService } from './../services/auth.service';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-<<<<<<< HEAD
-authService = inject(AuthService)
-=======
   authService = inject(AuthService);
->>>>>>> 99e039d64768339d96d5f6bb34cb97fd62edc1db
 }
