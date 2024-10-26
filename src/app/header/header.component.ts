396ce0b2418ed4bd4d0bef21f17db3f3ccd169a5
Fabian-85 <fabian.maier0805@hotmail.com>
2024-10-26 17:54:03 +0200

    add profil

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,15 +6,37 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from './../services/auth.service';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { ProfilLogoutButtonsComponent } from '../dialogs/profil-logout-buttons/profil-logout-buttons.component';
+import { MyProfilComponent } from '../dialogs/my-profil/my-profil.component';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatInputModule, MatIconModule, MatFormFieldModule, FormsModule, ],
+  imports: [CommonModule, MatInputModule, MatIconModule, MatFormFieldModule, FormsModule, MatDialogModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   authService = inject(AuthService);
+  readonly dialog = inject(MatDialog);
+  
+
+  openDialog() {
+    const dialogRef = this.dialog.open(ProfilLogoutButtonsComponent,{
+      width: '70px',
+      position: { top: '136px', right: '0' },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result='profil') {
+        const dialogRef = this.dialog.open(MyProfilComponent,{
+        });
+      }
+    });
+
+    
+  }
+
 }
