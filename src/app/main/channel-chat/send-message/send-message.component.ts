5bc08455364f94f70d5d54393a497edc4e56d8bb
Sebastian Torney <STorney@web.de>
2024-10-26 16:55:15 +0200

    thread component first html+css

--- src/app/main/channel-chat/send-message/send-message.component.ts
+++ src/app/main/channel-chat/send-message/send-message.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-send-message',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './send-message.component.scss'
 })
 export class SendMessageComponent {
-
+  @Input() placeholder: string = '';
 }
--- src/app/main/thread/thread.component.ts
+++ src/app/main/thread/thread.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SendMessageComponent } from '../channel-chat/send-message/send-message.component';
+import { MessageThreadComponent } from '../channel-chat/message-thread/message-thread.component';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+  imports: [SendMessageComponent, MessageThreadComponent],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
