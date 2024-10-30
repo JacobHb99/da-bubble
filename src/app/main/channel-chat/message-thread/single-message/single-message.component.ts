fca2738267fefa7ca3fe007c1b17839b7303d038
Sebastian Torney <STorney@web.de>
2024-10-30 20:00:51 +0100

    start reply message style

--- src/app/main/channel-chat/message-thread/single-message/single-message.component.ts
+++ src/app/main/channel-chat/message-thread/single-message/single-message.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-message',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './single-message.component.html',
   styleUrl: './single-message.component.scss'
 })
 export class SingleMessageComponent {
-
+  messageIsMine: boolean = false;
+  
 }
