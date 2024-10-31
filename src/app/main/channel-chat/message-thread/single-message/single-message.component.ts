e43ddcf9bbcfbf10e7456948c90b8737aa06363f
Sebastian Torney <STorney@web.de>
2024-10-31 19:09:36 +0100

    single-message add right involved user

--- src/app/main/channel-chat/message-thread/single-message/single-message.component.ts
+++ src/app/main/channel-chat/message-thread/single-message/single-message.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserDataService } from '../../../../services/user.service';
 
 @Component({
   selector: 'app-single-message',
@@ -10,5 +11,27 @@ import { CommonModule } from '@angular/common';
 })
 export class SingleMessageComponent {
   messageIsMine: boolean = false;
-  
+  user: any;
+  currentDate: string;
+
+
+  constructor(private userDataService: UserDataService) {
+    this.userDataService.selectedUser.subscribe((user) => {
+      this.user = user;
+    });
+
+    this.currentDate = this.getCurrentDate();
+  }
+
+  getCurrentDate() {
+    const days = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+    const months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli",
+      "August", "September", "Oktober", "November", "Dezember"];
+
+    const today = new Date();
+    const dayName = days[today.getDay()];
+    const day = today.getDate();
+    const monthName = months[today.getMonth()];
+    return `${dayName}, ${day}. ${monthName}`;
+  }
 }
