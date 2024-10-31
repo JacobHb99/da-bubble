b9ee4cffcff7c7a3008fa52cc40477ac776d3edd
Sebastian Torney <STorney@web.de>
2024-10-31 18:29:46 +0100

    channel header show userchat or channelchat

--- src/app/main/channel-chat/channel-chat.component.ts
+++ src/app/main/channel-chat/channel-chat.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { SendMessageComponent } from './send-message/send-message.component';
 import { MessageThreadComponent } from './message-thread/message-thread.component';
 import { UserDataService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-channel-chat',
   standalone: true,
-  imports: [SendMessageComponent, MessageThreadComponent],
+  imports: [SendMessageComponent, MessageThreadComponent,CommonModule],
   templateUrl: './channel-chat.component.html',
   styleUrl: './channel-chat.component.scss'
 })
 export class ChannelChatComponent {
   user: any;
+  isChannelChat: boolean = false;
 
   constructor(private userDataService: UserDataService) {
     this.userDataService.selectedUser.subscribe((user) => {
