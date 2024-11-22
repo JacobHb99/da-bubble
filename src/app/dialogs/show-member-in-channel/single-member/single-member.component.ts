a88d2f7ea72970833a370241226b499f02de32bb
Fabian-85 <fabian.maier0805@hotmail.com>
2024-11-22 18:57:31 +0100

    show member from a channel

--- src/app/dialogs/show-member-in-channel/single-member/single-member.component.ts
+++ src/app/dialogs/show-member-in-channel/single-member/single-member.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-single-member',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './single-member.component.scss'
 })
 export class SingleMemberComponent {
-
+ @Input() member: any;
 }
