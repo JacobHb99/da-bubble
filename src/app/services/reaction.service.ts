df4a95145d7ced1e23f64e4cc99ff13de110d91d
Sebastian Torney <STorney@web.de>
2024-11-29 19:34:22 +0100

    reaction.serv. -> delete old trys

--- src/app/services/reaction.service.ts
+++ src/app/services/reaction.service.ts
@@ -19,77 +19,6 @@ export class ReactionService {
 
   constructor() { }
 
-  // async updateMessageWithReaction(currentReaction: Reaction, msgId: string, conv: Conversation) {
-  //   const reactionId: string = currentReaction.id;
-  //   const username: string = this.authService.currentUserSig()?.username as string;
-  //   const convRef = doc(this.firestore, `conversations/${conv.conId}`);
-  //   const conversationSnapshot = await getDoc(convRef);
-
-  //   if (!conversationSnapshot.exists()) {
-  //     console.error("Conversation not found");
-  //     return;
-  //   }
-
-  //   // Lade die vorhandenen Nachrichten
-  //   const conversationData = conversationSnapshot.data();
-  //   const messages = conversationData['messages'];
-
-  //   // Nachricht anhand msgId finden
-  //   const messageIndex = messages.findIndex((msg: any) => msg.msgId === msgId);
-  //   if (messageIndex === -1) {
-  //     console.error("Message not found");
-  //     return;
-  //   }
-
-  //   const message = messages[messageIndex];
-
-  //   // Reaktion anhand reactionId finden
-  //   let reactionIndex = message.reactions.findIndex((reaction: any) => reaction.id === reactionId);
-
-  //   let reaction;
-  //   if (reactionIndex === -1) {
-  //     // Falls keine Reaktion mit dieser ID existiert, erstelle eine neue
-
-  //     reaction = {
-  //       id: reactionId,
-  //       reactedUser: { [username]: true },
-  //       counter: 1,
-  //     };
-  //     message.reactions.push(reaction);
-  //   } else {
-  //    let reactionRef = message.reactions[reactionIndex];
-
-  //     // Überprüfen, ob Benutzer bereits reagiert hat, und entsprechend hinzufügen/entfernen
-  //     if (reactionRef.reactedUser[username]) {
-  //       // Benutzer entfernen
-  //       delete reactionRef.reactedUser[username];
-  //       reactionRef.counter = Object.keys(reactionRef.reactedUser).length;
-
-  //       // Falls keine Benutzer mehr übrig sind, entferne die Reaktion
-  //       if (reactionRef.counter === 0) {
-  //         message.reactions.splice(reactionIndex, 1);
-  //       }
-  //     } else {
-  //       // Benutzer hinzufügen
-  //       reactionRef.reactedUser[username] = true;
-  //       reactionRef.counter = Object.keys(reactionRef.reactedUser).length;
-  //     }
-
-  //     // Aktualisiere die Reaktion im Array
-  //     if (reactionRef.counter > 0) {
-  //       message.reactions[reactionIndex] = reaction;
-  //     }
-  //   }
-
-  //   // Nachricht im messages-Array aktualisieren
-  //   messages[messageIndex] = message;
-
-  //   // Aktualisiertes messages-Array in Firestore speichern
-  //   await updateDoc(convRef, { messages });
-  //   console.log("Reaction updated successfully");
-  // }
-
-
   async getConvData(convNeedet: 'data' | 'ref') {
     const conv = this.fiBaService.currentConversation;
     const convRef = doc(this.firestore, `conversations/${conv.conId}`);
@@ -113,7 +42,7 @@ export class ReactionService {
     const msgId = currentMessage.msgId
 
     const messages = conversationData['messages'];
-    
+
     const messageIndex = this.findMessageIndex(messages, msgId);
     if (messageIndex === -1) {
       console.error("Message not found");
@@ -135,23 +64,14 @@ export class ReactionService {
 
 
   async updateMessageWithReaction(emoji: any, currentMessage: Message) {
-    const conversationData: any = await this.getConvData('data')
+
     let newReaction = this.createNewReaction(emoji)
     const username = this.authService.currentUserSig()?.username as string;
-    //const conv = this.fiBaService.currentConversation;
-    const msgId = currentMessage.msgId
-
-    // const convRef = doc(this.firestore, `conversations/${conv.conId}`);
-    // const conversationSnapshot = await getDoc(convRef);
-
-    // if (!conversationSnapshot.exists()) {
-    //   console.error("Conversation not found");
-    //   return;
-    // }
-
-    // const conversationData = conversationSnapshot.data();
+    const msgId = currentMessage.msgId;
+    const conversationData: any = await this.getConvData('data')
     const messages = conversationData['messages'];
-    console.log('conversationData', conversationData)
+    //console.log('conversationData', conversationData)
+
     // Nachricht finden
     const messageIndex = this.findMessageIndex(messages, msgId);
     if (messageIndex === -1) {
@@ -247,54 +167,4 @@ export class ReactionService {
       }
     });
   }
-
-
-
-
-
-
-
-
-  // createNewReaction(id: any) {
-  //   const username = this.authService.currentUserSig()?.username as string;
-
-  //   return new Reaction({
-  //     counter: 1,
-  //     id: id,
-  //     reactedUser: {
-  //       [`${username}`]: true
-  //     }
-  //   });
-  // }
-
-  // addUserToArray(reaction: Reaction, username: string) {
-  //   console.log('AddUserToreaction',reaction)
-  //   if (!reaction.reactedUser.includes(username)) {
-  //     console.log('ADDUSER9561651')
-  //     reaction.reactedUser.push(username);
-  //     reaction.counter = reaction.reactedUser.length +1;
-  //     console.log('UpdatedReaction',reaction)
-  //   }
-  //   // if (!this.reactedUser.includes(username)) {
-  //   //   this.reactedUser.push(username);
-  //   //   this.counter = this.reactedUser.length;
-  //   // }
-  // }
-
-  // removeUserFromArray(reaction: Reaction, username: string) {
-  //   const filteredUser = reaction.reactedUser.filter(user => user !== username);
-  //   reaction.counter = filteredUser.length-1;
-  //   // this.reactedUser = this.reactedUser.filter(user => user !== username);
-  //   // this.counter = this.reactedUser.length;
-  // }
-
-  // getCleanJSON(reaction: Reaction) {
-  //   return {
-  //     counter: reaction.counter,
-  //     id: reaction.id,
-  //     reactedUser: reaction.reactedUser
-  //   }
-  // }
-
-
 }
