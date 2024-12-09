d3fd7f1c4d34ae0ff597386c3effc8db5b811786
Fabian-85 <fabian.maier0805@hotmail.com>
2024-12-09 19:21:22 +0100

    js doc add-channel

--- src/app/dialogs/add-channel/add-channel.component.ts
+++ src/app/dialogs/add-channel/add-channel.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FirebaseService } from '../../services/firebase.service';
-import { MatDialog, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { AddPeopleComponent } from '../add-people/add-people.component';
 import { FormsModule } from '@angular/forms';
 import { ChannelService } from '../../services/channel.service';
@@ -12,41 +16,37 @@ import { ConversationService } from '../../services/conversation.service';
   standalone: true,
   imports: [CommonModule, MatDialogModule, FormsModule],
   templateUrl: './add-channel.component.html',
-  styleUrl: './add-channel.component.scss'
+  styleUrl: './add-channel.component.scss',
 })
 export class AddChannelComponent {
   isHoveredClose = false;
-  channelName = "";
-  inputTitle: string = "";
-  inputDesc: string = "";
-  
+  channelName = '';
+  inputTitle: string = '';
+  inputDesc: string = '';
 
-  constructor(public firebaseService: FirebaseService, public dialogRef: MatDialogRef<AddChannelComponent>, public dialog: MatDialog, public channelService: ChannelService){
+  constructor(
+    public firebaseService: FirebaseService,
+    public dialogRef: MatDialogRef<AddChannelComponent>,
+    public dialog: MatDialog,
+    public channelService: ChannelService
+  ) {
     this.firebaseService.selectedUsers = [];
   }
 
-
   closeDialogChannel(): void {
-    this.dialogRef.close(); 
+    this.dialogRef.close();
   }
 
-  
-
-  
-    openDialogAddPeople(): void {
-      this.channelService.currentChannel.title = this.inputTitle;
-      this.channelService.currentChannel.description = this.inputDesc;
-     
-      const dialogRef = this.dialog.open(AddPeopleComponent, {
-        width: '100%',
-        maxWidth: '720px'
-        
-      });
-      this.closeDialogChannel();
-      dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed');
-      });
-    }
-  
- 
+   /**
+   * open dialog for add a new channel
+   */
+  openDialogAddPeople(): void {
+    this.channelService.currentChannel.title = this.inputTitle;
+    this.channelService.currentChannel.description = this.inputDesc;
+    const dialogRef = this.dialog.open(AddPeopleComponent, {
+      width: '100%',
+      maxWidth: '720px',
+    });
+    this.closeDialogChannel();
+  }
 }
